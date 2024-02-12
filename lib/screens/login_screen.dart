27bd1815b2d9f2575d0b1a8a3eda42c06a6f9ab7
Shreyash D. Dhasade <2021.shreyash.dhasade@ves.ac.in>
2024-02-12 12:21:43 +0530

    added login screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,29 @@
+import "package:flutter/material.dart";
+import "package:synchwrite/colors.dart";
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton.icon(
+          onPressed: () {},
+          icon: Image.asset(
+            'assets/images/g-logo-2.png',
+            height: 20,
+          ),
+          label: const Text(
+            'Sign in with Google',
+            style: TextStyle(color: kBlackColor),
+          ),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kWhiteColor,
+            minimumSize: const Size(150, 50),
+          ),
+        ),
+      ),
+    );
+  }
+}
